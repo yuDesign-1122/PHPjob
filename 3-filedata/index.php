0975a5af7cfdcefc1daffa9b264acf33d0c07cc6
yuDesign-1122 <116777382+yuDesign-1122@users.noreply.github.com>
2022-12-20 11:19:38 +0900

    3章「ファイルのデータを読み書きする」
    チャレンジ問題

--- 3-filedata/index.php
+++ 3-filedata/index.php
@@ -11,4 +11,24 @@ if (is_writable($testFile)) {
 }
 echo '<br>';
 
+if (is_writable($testFile)) {
+  $fp = fopen ($testFile, "a");
+  fwrite ($fp, $contents);
+  fclose ($fp);
+  echo "finish writing!!";
+} else {
+  echo "not writable!";
+  exit;
+}
+echo '<br>';
+
+$test_file = "test2.txt";
+
+if (is_readable ($test_file)) {
+  $fp = fopen ($test_file, "r");
+  while ($line = fgets ($fp)) {
+    echo $line. '<br>';
+    exit;
+  }
+}
 ?>
---
+++ test/index.php
@@ -0,0 +1,4 @@
+<?php
+echo 'Good morning!';
+
+?>
