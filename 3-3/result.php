2b59043eb735a0741c15ad35f4e8a5514af28e6e
yuDesign-1122 <116777382+yuDesign-1122@users.noreply.github.com>
2023-01-06 08:46:20 +0900

    練習問題２再提出

--- 3-3/result.php
+++ 3-3/result.php
@@ -1,41 +1,70 @@
 <?php
+// $numbers = $_GET ['numbers'];
+// $number = $numbers[1];
+
+// echo date ("Y/m/d", strtotime ("today")). 'の運勢は'. '<br>';
+// echo '選ばれた数字は'. $number. '<br>';
+
+// switch ($number) {
+//     case '0';
+//     print '凶';
+//     break;
+//     case '1';
+//     print '小吉';
+//     break;
+//     case '2';
+//     print '小吉';
+//     break;
+//     case '3';
+//     print '小吉';
+//     break;
+//     case '4';
+//     print '中吉';
+//     break;
+//     case '5';
+//     print '中吉';
+//     break;
+//     case '6';
+//     print '中吉';
+//     break;
+//     case '7';
+//     print '吉';
+//     break;
+//     case '8';
+//     print '吉';
+//     break;
+//     case '9';
+//     print '大吉';
+//     break;
+// }
+
+// やり直し
 $numbers = $_GET ['numbers'];
-$number = $numbers[1];
-
+$number = str_split($numbers);
+// var_dump($number);
+// echo '<br>';
+// echo $number[array_rand($number)]. '<br>';
+$value = $number[array_rand($number)];
+// echo $value. '<br>';
 echo date ("Y/m/d", strtotime ("today")). 'の運勢は'. '<br>';
-echo '選ばれた数字は'. $number. '<br>';
+echo '選ばれた数字は'. $value. '<br>';
 
-switch ($number) {
-    case '0';
-    print '凶';
+switch($value) {
+    case $value == 0;
+    echo '凶';
     break;
-    case '1';
-    print '小吉';
+    case $value >= 1 && $value <= 3;
+    echo '小吉';
     break;
-    case '2';
-    print '小吉';
+    case $value >= 4 && $value <= 6;
+    echo '中吉';
     break;
-    case '3';
-    print '小吉';
+    case $value >= 7 && $value <= 8;
+    echo '吉';
     break;
-    case '4';
-    print '中吉';
+    case $value == 9;
+    echo '大吉';
     break;
-    case '5';
-    print '中吉';
-    break;
-    case '6';
-    print '中吉';
-    break;
-    case '7';
-    print '吉';
-    break;
-    case '8';
-    print '吉';
-    break;
-    case '9';
-    print '大吉';
-    break;
+    
 }
-
 ?>
